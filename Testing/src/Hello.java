23d23e35b98ce747b67efdf128d7fa08d6822214
hrx876 <hrx876@N0000A047.cs.utsarr.net>
2020-10-04 22:02:39 -0500

    Testing again for lab5

--- Testing/src/Hello.java
+++ Testing/src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello editing from eclipse");
 		System.out.println("Do you see all of this");
+		
+		//Print statement with full name and hobby
+		System.out.println("My name is Bethany Salazar and I like to explore new trails with my dog.");
 
 	}
 
